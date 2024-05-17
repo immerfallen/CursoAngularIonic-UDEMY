b5e0f7485b4c64adeaf60a1bca24c7045ac6568a
immerfallen <marodemelo10@gmail.com>
2024-05-17 09:34:32 -0300

    Estilizando a tela de discover

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -10,19 +10,26 @@ export class PlacesService {
       'p1',
       'Manahattan Mansion',
       'In the heart of New Yourk City',
-      'https://media.tatler.com/photos/6256d9edb3a3bbd05d93c198/master/w_1600,c_limit/LanierHouse_13042022_123%20E%2035th%20Street%20-%20New%20York,%20NY-2.jpg',
+      'https://thumbs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
       249.90
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Toujours',
+      'A romantic place in Paris',
+      'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSZyDvGG91YFGXqLY3Gt38Y6AEhtI9qKzGGaimzN3shUA11aGwmymMu7Wwv6CQ3DMQh',
+      389.90
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Foggy_Day_Neuschwanstein_Castle_%28229936735%29.jpeg/800px-Foggy_Day_Neuschwanstein_Castle_%28229936735%29.jpeg?20181015014702',
-      389.90
+      99.99
     ),
   ];
 
-  get places() {
+get places() {
     return [...this._places];
   }
 

d60ae2d76ecaea8ad8434ae527073e9734534557
immerfallen <marodemelo10@gmail.com>
2024-05-19 11:02:02 -0300

    Criado loading overlay para login

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
 })
-export class AuthPage  {
-  constructor(private authService: AuthService, private router: Router) {}
+export class AuthPage {
+  isLoading = false;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadCtlr: LoadingController
+  ) {}
 
   onLogin() {
     this.authService.login();
-    this.router.navigateByUrl('/places/tabs/discover')
-  }  
+    this.isLoading = true;
+    this.loadCtlr.create({keyboardClose: true, message: 'Logging in...'}).then(loadingEl=> {
+      loadingEl.present();
+      setTimeout(() => {
+        this.isLoading = false;
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/places/tabs/discover');
+      }, 1500);
+    })
+   
+  }
 }

c70fd6d58096e8ee62114f896987d1aa34cde910
immerfallen <marodemelo10@gmail.com>
2024-05-19 10:01:55 -0300

    Criado slide para bookings

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from './booking.service';
+import { Booking } from './booking.model';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
 })
-export class BookingsPage  {
+export class BookingsPage implements OnInit {
+loadedBookings: Booking[] = [];
 
-  constructor() { }
+  constructor(private bookingService: BookingService) { }
+
+  ngOnInit() {
+    this.loadedBookings = this.bookingService.bookings;
+  }
+
+  onCancelBooking(id: string, slidingItem: IonItemSliding){
+slidingItem.close();
+
+  }
 
   
 

871e9aa8213550d9f9273bed3b32a8a104e78802
immerfallen <marodemelo10@gmail.com>
2024-05-20 13:31:40 -0300

    criado ts do reactive form das offers

--- src/app/places/offers/new-offer/new-offer.module.ts
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { NewOfferPage } from './new-offer.page';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
     path: '',
-    component: NewOfferPage
-  }
+    component: NewOfferPage,
+  },
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
-  declarations: [NewOfferPage]
+  declarations: [NewOfferPage],
 })
 export class NewOfferPageModule {}
--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -9,4 +9,8 @@ export class NewOfferPage  {
 
   constructor() { } 
 
+  onCreateOffer(){
+    
+  }
+
 }

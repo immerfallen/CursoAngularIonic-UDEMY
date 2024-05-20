ac3c092b39f1734dca2c40139ac0aa7afa2f4e6a
immerfallen <marodemelo10@gmail.com>
2024-05-20 11:01:13 -0300

    criado form para offer

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,23 +11,45 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage {
   isLoading = false;
+  isLogin = true;
+
   constructor(
     private authService: AuthService,
     private router: Router,
-    private loadCtlr: LoadingController
-  ) {}
+    private loadingCtrl: LoadingController
+  ) {}  
 
   onLogin() {
-    this.authService.login();
     this.isLoading = true;
-    this.loadCtlr.create({keyboardClose: true, message: 'Logging in...'}).then(loadingEl=> {
-      loadingEl.present();
-      setTimeout(() => {
-        this.isLoading = false;
-        loadingEl.dismiss();
-        this.router.navigateByUrl('/places/tabs/discover');
-      }, 1500);
-    })
-   
+    this.authService.login();
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Logging in...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/places/tabs/discover');
+        }, 1500);
+      });
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
 }
